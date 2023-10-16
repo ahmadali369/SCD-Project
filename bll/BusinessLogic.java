cb86795d7a304b7803d3bcdbfd99f888f0aa2eef
ahmadali369 <ahmadali.ashfaq786@gmail.com>
2023-10-16 17:14:01 +0000

    feat - added business logic functions in bll

---
+++ bll/BusinessLogic.java
@@ -0,0 +1,25 @@
+package dll;
+import java.utils.*;
+
+class Businesslogic {
+
+
+  public Map<String, Boolean> showAllBooks(){
+
+    Map<String, Boolean> books = new HashMap<>();
+    return books;
+    
+  }
+
+  public boolean addBook(String seriesName, String authorName){
+    return true;
+}
+
+
+ public boolean updateBook(String seriesName, String bookName, String authorName){}
+  
+  public boolean deleteBook(String seriesName, String authorName){}
+
+  
+  
+}
